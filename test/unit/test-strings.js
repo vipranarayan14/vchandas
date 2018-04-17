2afd15032eaeccb9d64663d8b66c1d6ddb1cd7bb
Prasanna Venkatesh T S <vipranarayan14@gmail.com>
2018-04-17 18:54:18 +0530

    add 'ganas' to each of 'testStrings' for testing 'getGanas' module.

--- test/unit/test-strings.js
+++ test/unit/test-strings.js
@@ -1,56 +1,67 @@
 export const testStrings = [
 
   {
+    ganas: '',
     matras: [],
     string: 'ग्',
     syllables: []
   },
   {
+    ganas: 'ga',
     matras: ['ga'],
     string: 'नं',
     syllables: ['नं']
   },
   {
+    ganas: 'la',
     matras: ['la'],
     string: 'क्ष्त्र्ज्ञ',
     syllables: ['क्ष्त्र्ज्ञ']
   },
   {
+    ganas: 'la,la',
     matras: ['la', 'la'],
     string: 'इव',
     syllables: ['इ', 'व']
   },
   {
+    ganas: 'ga,la',
     matras: ['ga', 'la'],
     string: 'सोऽपि',
     syllables: ['सो', 'पि']
   },
   {
+    ganas: 'ra',
     matras: ['ga', 'la', 'ga'],
     string: 'रावणः',
     syllables: ['रा', 'व', 'णः']
   },
   {
+    ganas: 'ra',
     matras: ['ga', 'la', 'ga'],
     string: 'नम्रता',
     syllables: ['न', 'म्र', 'ता']
   },
   {
+    ganas: 'ta|ga',
     matras: ['ga', 'ga', 'la', 'ga'],
     string: 'रामायणम्',
     syllables: ['रा', 'मा', 'य', 'णम्']
   },
   {
+    ganas: 'sa|ga',
     matras: ['la', 'la', 'ga', 'ga'],
     string: 'न निमग्नं',
     syllables: ['न', 'नि', 'म', 'ग्नं']
   },
   {
+    ganas: 'ra',
     matras: ['ga', 'la', 'ga'],
     string: 'क्रन्दनम्',
     syllables: ['क्र', 'न्द', 'नम्']
   },
   {
+    ganas: 'sa|sa|sa|sa',
     matras: ['la', 'la', 'ga', 'la', 'la', 'ga', 'la', 'la', 'ga', 'la', 'la', 'ga'],
     string: 'विदिताखिलशास्त्रसुधाजलधे',
     syllables: [
@@ -58,6 +69,7 @@ export const testStrings = [
     ]
   },
   {
+    ganas: 'ya|ya|ya|ya',
     matras: ['la', 'ga', 'ga', 'la', 'ga', 'ga', 'la', 'ga', 'ga', 'la', 'ga', 'ga'],
     string: 'मुखान्निःसरन्ते गिरश्चापि चित्रम्',
     syllables: [
@@ -65,6 +77,7 @@ export const testStrings = [
     ]
   },
   {
+    ganas: 'ya|ya|ya|ya',
     matras: ['la', 'ga', 'ga', 'la', 'ga', 'ga', 'la', 'ga', 'ga', 'la', 'ga', 'ga'],
     string: 'न जानामि शब्दं न जानामि चार्थं',
     syllables: [
