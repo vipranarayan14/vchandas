7416a4920b0b4da0695da2bc078d807230cda361
Prasanna Venkatesh T S <vipranarayan14@gmail.com>
2018-04-17 18:54:46 +0530

    add test for 'getGanas' module.

---
+++ test/unit/ganas.spec.js
@@ -0,0 +1,38 @@
+import { expect } from 'chai';
+
+import { getGanas } from '../../src/ganas';
+import { getMatras } from '../../src/matras';
+import { getSliceDetails } from '../../src/slice-details';
+import { makeSchemeTree } from '../../src/scheme-tree';
+import { schemes } from 'vtranslit-schemes';
+import { testStrings } from './test-strings';
+import { vTokenize } from 'vtokenize';
+
+const devanagariScheme = schemes.Deva;
+
+const {
+  schemeTree,
+  maxTokenLength
+} = makeSchemeTree(devanagariScheme);
+
+const getTokens = inStr =>
+
+  vTokenize(
+    inStr,
+    maxTokenLength,
+    getSliceDetails(schemeTree)
+  );
+
+describe('getGanas', () => {
+
+  testStrings.forEach(testString => {
+
+    it(`should return ganas for '${testString.string}' as '${testString.ganas}'`, () => {
+
+      expect(getGanas(getMatras(getTokens(testString.string)))).to.deep.equal(testString.ganas);
+
+    });
+
+  });
+
+});
