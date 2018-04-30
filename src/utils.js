e8c88bb7062e5260bc5c96776b6c1bf1523adff4
Prasanna Venkatesh T S <vipranarayan14@gmail.com>
2018-04-30 14:30:55 +0530

    fix incorrect regex. add 'removeSpaces' function to remove space-tokens from tokens made by 'vTokenize'.

--- src/utils.js
+++ src/utils.js
@@ -1,3 +1,7 @@
 export const cleanString = str =>
 
-  str.slice(0).trim().replace(/\s\s+/g, ' ');
+  str.slice(0).trim().replace(/\s\s*/g, ' ');
+
+export const removeSpaces = tokens =>
+
+  tokens.filter(token => token.type !== 'space');
